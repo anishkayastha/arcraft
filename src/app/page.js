f539561ce2ee4f0009e18b0fd6ba3afc39df962f
anish kayastha <anish@GEONET.COM>
2024-02-18 16:51:04 +0545

    added howitworks

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
+import HowItWorks from '@/components/HowItWorks'
+import Testimonial from '@/components/Testimonial'
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Hero className="pt-32 pb-0 lg:pb-10" />
+      <About className="py-16 lg:py-32 overflow-hidden" />
+      <HowItWorks className="py-16 !pt-32 lg:py-32" />
+      <Testimonial className="py-16 lg:py-32" />
     </>
   );
 }
